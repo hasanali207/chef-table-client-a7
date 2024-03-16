98730ee59e0c68a9229e7360c050c07dd48e6e0a
hasanali207 <hasanali142228@gmail.com>
2024-03-16 20:14:10 +0600

    added recipe item

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import Bookmark from "./components/Bookmark"
-import Header from "./components/Header"
-import Recipe from "./components/Recipe"
+import Header from "./components/Header";
+
+import { useEffect } from "react";
+import { useState } from "react";
+import Recipe from "./components/Recipe";
 
 function App() {
- 
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("./recipe.json")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  
 
   return (
     <>
       <div className="max-w-7xl mx-auto p-4">
-      <Header></Header>
-      <div>
-        <Recipe></Recipe>
-        <Bookmark></Bookmark>
-      </div>
+        <Header></Header>
+        <div className="flex justify-between ">
+          {/* left content  */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-2/3 ">
+          {
+            items.map(item => <Recipe key={item.recipe_id} item={item}></Recipe>)
+          }
+          </div>
+
+          {/* right content  */}
+          <div className="w-1/3">
+            <h1>this is heading</h1>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,16 +1,58 @@
+import PropTypes from "prop-types";
+import { GoClock } from "react-icons/go";
 
-import PropTypes from 'prop-types';
 
-const Recipe = () => {
-    return (
-        <div>
-            
+const Recipe = ({ item }) => {
+  const {
+    recipe_name,
+    short_description,
+    ingredients,
+    preparing_time,
+    calories,
+    recipe_image,
+  } = item;
+
+  return (
+    
+      <div className="card  bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={recipe_image} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{recipe_name}</h2>
+          <p>{short_description}</p>
+          
         </div>
-    );
+       <div className="p-4">
+       <div className="border-y-2 py-3">
+          <h3 className="text-3xl font-normal mb-3">
+            Ingredient {ingredients.length}
+          </h3>
+          {ingredients.map((ingredientsItem, idx) => (
+            <li className="ml-2" key={idx}>
+              
+              {ingredientsItem}
+            </li>
+          ))}
+        </div>
+       </div>
+        <div className="p-4">
+          <div className="flex justify-between items-center">
+            <h3 className="flex justify-center items-center gap-2"> <GoClock></GoClock> {preparing_time} Minutes</h3>
+            <h3>{calories} Calories</h3>
+          </div>
+            
+            <div className="flex justify-center items-center"> 
+            <button className="btn btn-primary my-4">Want to Cook</button>
+            </div>
+          </div>
+      </div>
+    
+  );
 };
 
 Recipe.propTypes = {
-    
+  item: PropTypes.object.isRequired,
 };
 
 export default Recipe;
