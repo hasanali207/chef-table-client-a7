f2a33ff89eea8e5f81ff95cf737a2e537c4af696
hasanali207 <hasanali142228@gmail.com>
2024-03-16 22:52:38 +0600

    added item section

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,24 @@ function App() {
     <>
       <div className="max-w-7xl mx-auto p-4">
         <Header></Header>
+
+        <div className="text-center p-20"> 
+          <h1 className=" text-5xl text-[#150B2B] font-semibold mb-4">Our Recipes</h1>
+          <p className="text-[#150B2B60]">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. <br></br> Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+
+        </div>
         <div className="flex justify-between ">
+
+          
           {/* left content  */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-2/3 ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-2/3 ">
           {
             items.map(item => <Recipe key={item.recipe_id} item={item}></Recipe>)
           }
           </div>
 
           {/* right content  */}
-          <div className="w-1/3">
+          <div className="w-1/3 p-6">
             <h1>this is heading</h1>
           </div>
         </div>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { GoClock } from "react-icons/go";
+import { FaGripfire } from "react-icons/fa6";
+
 
 
 const Recipe = ({ item }) => {
@@ -14,22 +16,22 @@ const Recipe = ({ item }) => {
 
   return (
     
-      <div className="card  bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
+      <div className="card border-[#28280420] border">
+        <figure className="px-6 pt-6">
           <img src={recipe_image} alt="Shoes" className="rounded-xl" />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{recipe_name}</h2>
-          <p>{short_description}</p>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-3 text-[#282828]">{recipe_name}</h2>
+          <p className="text-[#878787]">{short_description}</p>
           
         </div>
        <div className="p-4">
        <div className="border-y-2 py-3">
-          <h3 className="text-3xl font-normal mb-3">
+          <h3 className="text-xl font-medium mb-3">
             Ingredient {ingredients.length}
           </h3>
           {ingredients.map((ingredientsItem, idx) => (
-            <li className="ml-2" key={idx}>
+            <li className="ml-2 text-[#878787]" key={idx}>
               
               {ingredientsItem}
             </li>
@@ -39,12 +41,13 @@ const Recipe = ({ item }) => {
         <div className="p-4">
           <div className="flex justify-between items-center">
             <h3 className="flex justify-center items-center gap-2"> <GoClock></GoClock> {preparing_time} Minutes</h3>
-            <h3>{calories} Calories</h3>
+            <h3 className="flex justify-center items-center gap-2" > <FaGripfire></FaGripfire> {calories} Calories</h3>
+            
           </div>
             
-            <div className="flex justify-center items-center"> 
-            <button className="btn btn-primary my-4">Want to Cook</button>
-            </div>
+          <button className=" bg-[#0BE58A] py-3 px-4 font-medium rounded-2xl  my-4">Want to Cook</button>
+            
+            
           </div>
       </div>
     
