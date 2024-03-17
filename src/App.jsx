3b3f22e9d9ddadb0729aefcceb46afaba8a51730
hasanali207 <hasanali142228@gmail.com>
2024-03-17 19:47:24 +0600

    added second bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Recipe from "./components/Recipe";
 import Bookmark from "./components/Bookmark";
+import Cookbookmark from "./components/Cookbookmark";
 
 function App() {
   const [items, setItems] = useState([]);
   const [bookmark, setBookmark] = useState([]);
+  
+  const [cookBookmark, setCookBookmark] = useState([]);
+
+
+
 
   useEffect(() => {
     fetch("./recipe.json")
@@ -23,7 +29,19 @@ const bookmarkItem = (item) =>{
  }
   
 }
-console.log(bookmark);
+
+
+const deleteBookmark = (id) =>{
+  const newBookmark = bookmark.filter(item => item.recipe_id != id)
+  setBookmark(newBookmark)
+    
+}
+
+
+const cookingBookmark = (item) =>{
+  setCookBookmark([...cookBookmark,item])
+ 
+}
 
 
 
@@ -46,14 +64,14 @@ console.log(bookmark);
           {/* left content  */}
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-[60%]">
             {items.map((item) => (
-              <Recipe key={item.recipe_id} item={item} bookmarkItem={bookmarkItem} ></Recipe>
+              <Recipe key={item.recipe_id} item={item}  bookmarkItem={bookmarkItem} ></Recipe>
             ))}
           </div>
 
           {/* right content  */}
-          <div className="py-6 w-[40%] border-[#28280420] border ">
+          <div className="w-[40%]">
             {/* table one  */}
-            <div className="top-bookmar">
+            <div className="top-bookmark border-[#28280420] border py-6">
               <h1 className="text-xl font-medium text-center mb-4">
                 Want To Cook <span>{bookmark.length}</span>
               </h1>
@@ -70,20 +88,50 @@ console.log(bookmark);
                 <tbody className="">
                   {/* row 1 */}
                   {
-                    bookmark.map((bk, idx) => <Bookmark idx={idx} key={idx} bookmark={bk}></Bookmark>)
+                    bookmark.map((bk, idx) => <Bookmark idx={idx} key={idx} bookmark={bk} deleteBookmark={deleteBookmark} cookingBookmark={cookingBookmark} ></Bookmark>)
                   }
                 </tbody>
               </table>
             </div>
 
             {/* bottom bookmark  */}
+            <div className="bottom-bookmark border-[#28280420] border py-6 mt-6">
+              <h1 className="text-xl font-medium text-center mb-4">
+                Currently Cooking <span>{cookBookmark.length}</span>
+              </h1>
+              <table className="table">
+                {/* head */}
+                <thead className="thead">
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Times</th>
+                    <th>Calories</th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {/* row 1 */}
+               
+                {
+                  cookBookmark.map((item, idx) => <Cookbookmark key={idx} idx={idx} bookmark={item}></Cookbookmark>
+                  )
+                }
+              
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <th>Total Times=  <span><br /></span>  0 Minutes </th>
+                    <th>Total Calories=   <span><br /></span> 0 Caories</th>
+                    
+                  </tr>
+                
+
+                </tbody>
+              </table>
+            </div>
+
 
             
-
-            <div>
-
-
-            </div>
           </div>
         </div>
       </div>
--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
 
-const Bookmark = ({ bookmark, idx }) => {
+const Bookmark = ({ bookmark, idx, deleteBookmark, cookingBookmark }) => {
+
+    
   return (
+
+
     <tr className="">
       <th>{idx + 1}</th>
       <td>{bookmark.recipe_name}</td>
       <td>{bookmark.preparing_time} Minutes</td>
       <td>{bookmark.calories} Clories</td>
       <td>
-        <button className="bg-[#0BE58A] py-2 px-2 font-medium rounded-2xl  my-4">
-          Preparing
-        </button>
+      <button
+  onClick={(event) => {
+    event.preventDefault();
+    deleteBookmark(bookmark.recipe_id);
+    cookingBookmark(bookmark);
+  }}
+  className="bg-[#0BE58A] py-2 px-2 font-medium rounded-2xl my-4"
+>
+  Preparing
+</button>
       </td>
     </tr>
   );
